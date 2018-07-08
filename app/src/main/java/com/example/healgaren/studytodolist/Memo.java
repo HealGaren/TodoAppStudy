8cc803160484a7030a9ef6959e53b16d90abc5c8
HealGaren <healgaren99@gmail.com>
2018-07-09 01:22:00 +0900

    메모 VO에 detailContent 추가

--- app/src/main/java/com/example/healgaren/studytodolist/Memo.java
+++ app/src/main/java/com/example/healgaren/studytodolist/Memo.java
@@ -3,10 +3,16 @@ package com.example.healgaren.studytodolist;
 public class Memo {
     private String title;
     private String content;
+    private String detailContent;
 
     public Memo(String title, String content) {
+        this(title, content, "");
+    }
+
+    public Memo(String title, String content, String detailContent) {
         this.title = title;
         this.content = content;
+        this.detailContent = detailContent;
     }
 
     public String getTitle() {
@@ -23,5 +29,13 @@ public class Memo {
 
     public void setContent(String content) {
         this.content = content;
+    }
+
+    public String getDetailContent() {
+        return detailContent;
+    }
+
+    public void setDetailContent(String detailContent) {
+        this.detailContent = detailContent;
     }
 }
