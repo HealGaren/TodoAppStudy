5e2136bbe2f15b6b6967bceebbe2ef9899793f3c
HealGaren <healgaren99@gmail.com>
2018-07-09 00:47:16 +0900

    리사이클러뷰와 어댑터, 메모 VO 만들고 목업 데이터 채워 연동시킴

--- app/src/main/java/com/example/healgaren/studytodolist/MainActivity.java
+++ app/src/main/java/com/example/healgaren/studytodolist/MainActivity.java
@@ -4,10 +4,17 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
+import java.util.Arrays;
+
 public class MainActivity extends AppCompatActivity {
+
+    RecyclerView memoRecycler;
+    MemoAdapter memoAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +31,21 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        memoRecycler = findViewById(R.id.recycler_memo);
+        memoAdapter = new MemoAdapter();
+        memoRecycler.setAdapter(memoAdapter);
+
+        memoRecycler.setLayoutManager(new LinearLayoutManager(this));
+
+        memoAdapter.addAll(Arrays.asList(
+                new Memo("양치하기", "양치는 잘 하자"),
+                new Memo("안드로이드 앱 개발", "스터디 하려면 해야지"),
+                new Memo("롤 하기", "가렌 S랭크를 찍어볼까?"),
+                new Memo("설거지 하기", "밥을 먹었으면 설거지를 해야지")
+        ));
+
+
     }
 
 }
---
+++ app/src/main/java/com/example/healgaren/studytodolist/MemoAdapter.java
@@ -0,0 +1,75 @@
+package com.example.healgaren.studytodolist;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+public class MemoAdapter extends RecyclerView.Adapter<MemoAdapter.ViewHolder> {
+
+    private List<Memo> memoList = new ArrayList<>();
+
+    public void add(Memo memo) {
+        memoList.add(memo);
+        notifyItemInserted(memoList.size() - 1);
+    }
+
+    public void addAll(Collection<Memo> memoCollection) {
+        int oldSize = memoList.size();
+        memoList.addAll(memoCollection);
+        notifyItemRangeInserted(oldSize, memoCollection.size());
+    }
+
+    public void remove(int position) {
+        memoList.remove(position);
+        notifyItemRemoved(position);
+    }
+
+    public void clear() {
+        int oldSize = memoList.size();
+        memoList.clear();
+        notifyItemRangeRemoved(0, oldSize);
+    }
+
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_memo, parent, false);
+        return new ViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        holder.bind(memoList.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return memoList.size();
+    }
+
+    static class ViewHolder extends RecyclerView.ViewHolder {
+
+        private TextView titleText;
+        private TextView contentText;
+
+        ViewHolder(View itemView) {
+            super(itemView);
+            titleText = itemView.findViewById(R.id.text_title);
+            contentText = itemView.findViewById(R.id.text_content);
+        }
+
+        void bind(Memo memo) {
+            titleText.setText(memo.getTitle());
+            contentText.setText(memo.getContent());
+        }
+
+    }
+}
